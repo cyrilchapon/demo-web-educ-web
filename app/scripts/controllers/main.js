8a8374ebc442998b9d796d57c36a4c4b21bd9793
Cyril CHAPON <cyril.chapon@live.com>
2016-07-31 13:06:32 +0200

    Clean trailing

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,5 @@
  */
 angular.module('demoWebEducWebApp')
   .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+
   });
