f7c276e9f78689098da8f28d9ef6267ddee6a6a2
Cyril CHAPON <cyril.chapon@live.com>
2016-08-02 16:19:58 +0200

    Fix dependencies for simple-file-input

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -30,7 +30,7 @@ module.exports = function(config) {
       'bower_components/angular-moment/angular-moment.js',
       'bower_components/angular-dynamic-locale/src/tmhDynamicLocale.js',
       'bower_components/angular-ui-router/release/angular-ui-router.js',
-      'bower_components/simple-angular-file-input/dist/angular-file-input.js',
+      'bower_components/simple-angular-file-input/dist/angular-file-input.min.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
